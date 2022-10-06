277425459c2b64ab3be0db75607a6e5ea61ed603
HardRoof <mateus_grandez@hotmail.com>
2022-10-06 10:44:38 -0600

    Added insert and delete methods

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ class Node {
     this.left = left;
     this.right = right;
   }
+
+  insertNode(data) {
+    if (this.data == data) return;
+    else if (this.data > data) {
+      if (this.left) this.left.insertNode(data);
+      else this.left = new Node(data);
+    } else {
+      if (this.right) this.right.insertNode(data);
+      else this.right = new Node(data);
+    }
+  }
+
+  deleteNode(data) {
+    if (data < this.data && this.left) this.left = this.left.deleteNode(data);
+    else if (data > this.data && this.right) this.right = this.right.deleteNode(data);
+    // When the two conditions fail, then data = this.data
+    else {
+      if (this.right && this.left) {
+        let minValue = this.right.findMinValue();
+        this.data = minValue;
+        this.right = this.right.deleteNode(minValue);
+      } else if (this.left) return this.left;
+      else if (this.right) return this.right;
+      else return null;
+    }
+    return this;
+  }
+
+  findMinValue() {
+    let current = this; // Root of right subtree
+    while (current.left) current = current.left;
+    return current.data;
+  }
 }
 
 class Tree {
@@ -23,5 +56,21 @@ class Tree {
     newNode.right = this.buildTree(secondHalf);
 
     return newNode;
+  }
+
+  // Insert values into tree
+  insert(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      this.root.insertNode(value);
+    }
+  }
+
+  // Delete values in the tree
+  delete(value) {
+    if (this.root) {
+      this.root = this.root.deleteNode(value);
+    }
   }
 }
